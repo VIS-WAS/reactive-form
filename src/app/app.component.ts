aad9f36cf0d53811486c7d8d00e9bdf32842f643
VIS-WAS <vishwas.ms8830@gmail.com>
2024-04-15 19:05:17 +0530

    introduction to reactive form and created reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-reactive-forms';
+
+  reactiveForm: FormGroup;
+
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl('Vishwas'),
+      lastname: new FormControl('MS'),
+      email: new FormControl(null),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+      street: new FormControl(null),
+      country: new FormControl('India'),
+      city: new FormControl(null),
+      region: new FormControl(null),
+      postal: new FormControl(null),
+    });
+  }
+  formSubmitted() {
+    console.log(this.reactiveForm);
+  }
 }
